6c8e9c068e6db6b619f742870532ed6cfbf32986
isaachouston <isaacprh@gmail.com>
2020-10-06 18:55:09 -0300

    Criando rotas do projeto

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Splash from '../pages/Splash';
+import Films from '../pages/Films';
+
+const Router = createStackNavigator();
+
+const Routes: React.FC = () => (
+  <Router.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+  >
+    <Router.Screen name="Splash" component={Splash} />
+    <Router.Screen name="Films" component={Films} />
+  </Router.Navigator>
+);
+
+export default Routes;
